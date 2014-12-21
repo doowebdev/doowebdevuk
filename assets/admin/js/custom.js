053ee76ae29a35bdabbb3e203bb707740ce52585
Freddie Annobil <freddie@doowebdev.com>
2014-12-21 00:46:19 +0000

    Initial

--- assets/admin/js/custom.js
+++ assets/admin/js/custom.js
@@ -32,30 +32,7 @@ $(document).ready(function(){
     }
   });
 
-/* Old Code 
 
-  $("#nav > li > a").on('click',function(e){
-      if($(this).parent().hasClass("has_sub")) {
-       
-		  e.preventDefault();
-
-		  if(!$(this).hasClass("subdrop")) {
-			// hide any open menus and remove all other classes
-			$("#nav li ul").slideUp(350);
-			$("#nav li a").removeClass("subdrop");
-			
-			// open our new menu and add the open class
-			$(this).next("ul").slideDown(350);
-			$(this).addClass("subdrop");
-		  }
-		  
-		  else if($(this).hasClass("subdrop")) {
-			$(this).removeClass("subdrop");
-			$(this).next("ul").slideUp(350);
-		  } 
-      }   
-      
-  }); */
 });
 
 $(document).ready(function(){
@@ -179,8 +156,6 @@ setTimeout(function(){
 	$('.progress-animated .progress-bar').each(function() {
 		var me = $(this);
 		var perc = me.attr("data-percentage");
-
-		//TODO: left and right text handling
 
 		var current_perc = 0;
 
